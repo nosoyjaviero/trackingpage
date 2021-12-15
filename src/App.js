2bc46aee829d3824b7a33f3342c0e0f33701e322
nosoyjaviero <lopez4935@gmail.com>
2021-12-15 17:18:52 -0600

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React, { useState, useEffect, Fragment } from 'react';
+import axios from 'axios';
+import NameForm from './NameForm'
+
+
+
+// una funcion que  obtiene una base de datos consultada  en una url en especifico
+// consulta por un seguimiento en especifico
+//devuelve el estado del pedido 
+//
+function Seguimiento(TRACKID) {
+
+  var baseUrl = "http://localhost/react/";
+
+  const [data, setData] = useState([]);
+  const peticionGet = async () => {
+    await axios.get(baseUrl)
+      .then(response => {
+        setData(response.data);
+
+      })
+  }
+
+  useEffect(() => {
+    peticionGet();
+  }, [])
+
+  var trackState = "";
+
+  data.find(function (value, index) {
+    if (value.track_id == TRACKID) {
+      trackState = value.Estado;
+    } else {
+    }
+  });
+  if (trackState == "") {
+    trackState = "No existe seguimiento"
+  }
+
+  return trackState;
 }
 
+
+//una funcion que consumi el api de compra y venta del banco de costarica
+//cesta funcion obtiene el dato diario
+//devuelve el precio y fecha en array
+let date;
+function API() {
+  date = new Date();
+  var compra = "";
+  var venta = "";
+  var fecha = "";
+  var baseUrl = `https://tipodecambio.paginasweb.cr/api//${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+
+  const [data, setData] = useState([]);
+  const peticionGet = async () => {
+    await axios.get(baseUrl)
+      .then(response => {
+        setData(response.data);
+      })
+  }
+  useEffect(() => {
+    peticionGet();
+  }, [])
+
+  compra = data.compra;
+  venta = data.venta;
+  fecha = data.fecha;
+
+  return [compra, venta, fecha]
+}
+
+
+//esqueleto de la pagina. Aqui se une los componentes y apis y funciones
+function App() {
+
+
+  return (
+
+    <div className="App">
+      <header className="header">
+        <h1>Bienvenido</h1>
+
+      </header >
+
+      <body>
+
+        <NameForm />
+
+
+        {Seguimiento(a)}
+        <div>
+          aqui si busca no importa lo que sea que le envia por parametro
+          no mas de pasarle el parametro
+          Por alguna razon, en formtracking no sirven las funciones
+          entonces no se le pueden pasar ningun tipo  parametro
+          menos uno de formulario
+        </div>
+
+      </body>
+
+      <footer>
+        <div className='cambiocr'>
+          <div>
+            <div>
+              API de tipo de cambio
+            </div>
+            {API()[2]}</div>
+          <div className='compra'>Compra: {API()[0]}</div>
+          <div className='venta'>Venta: {API()[1]}</div>
+        </div>
+      </footer>
+
+    </div>
+
+  );
+}
+var a = parseInt("4");
 export default App;
---
+++ src/NameForm.js
@@ -0,0 +1,107 @@
+
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios';
+
+
+
+
+
+function Seguimiento() {
+
+  var baseUrl = "http://localhost/react/";
+
+  const [data, setData] = useState([]);
+  const peticionGet = async () => {
+    await axios.get(baseUrl)
+      .then(response => {
+        setData(response.data);
+        
+      })
+  }
+
+  useEffect(() => {
+    peticionGet();
+  }, [])
+
+
+  var trackState = "";
+
+  data.find(function (value, index) {
+    if (value.track_id == a) {
+      trackState = value.Estado;
+    } else {
+    }
+  });
+  if (trackState == "") {
+    trackState = "No existe seguimiento"
+  }
+
+}
+
+
+var a ;
+
+class NameForm extends React.Component {
+
+  
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      nombre: '',
+     
+    }
+    this.onChange = this.onChange.bind(this);
+    this.enviarAlaBD = this.enviarAlaBD.bind(this);
+  }
+
+  onChange = e =>{
+    const {name, value} = e.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+  enviarAlaBD = e  => {
+    e.preventDefault();
+    let valido = true;
+    if(this.state.nombre === ''){
+      this.setState({
+        invalidNombre: true,
+        mensajeNombre: 'El campo nombre es obligatorio, indica un seguimiento',
+      });
+      valido = false;
+    }
+   
+    if(valido){
+      a=parseInt(this.state.nombre);
+      console.log(a);
+      
+    }
+  }
+  
+
+  render(){
+    return (
+      <div>
+        
+            
+            <form className='form-register' onSubmit={this.enviarAlaBD}>
+             
+                <input type="text" name="nombre" className='controls' placeholder='Digite su seguimiento' 
+                value={this.state.nombre} 
+                onChange={this.onChange}
+                 invalid={this.state.invalidNombre} />
+             
+              
+                <button className="botons" color="success">Buscar </button>
+              
+            </form>
+            {Seguimiento}
+      </div>
+
+    );
+    
+  }
+}
+export default NameForm;
