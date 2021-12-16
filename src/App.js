8443acb7475d4a3afb63950a1dde1ef478ca1f00
nosoyjaviero <lopez4935@gmail.com>
2021-12-16 11:35:04 -0600

    version 2.0

--- src/App.js
+++ src/App.js
@@ -74,8 +74,53 @@ function API() {
 }
 
 
+
+
 //esqueleto de la pagina. Aqui se une los componentes y apis y funciones
 function App() {
+
+  const [allData,setAllData] = useState([]);
+  const [filteredData,setFilteredData] = useState(allData);
+
+  const handleSearch = (event) => {
+    let value = event.target.value.toLowerCase();
+    let result = [];
+    console.log(value);
+    result = allData.filter((data) => {
+    return data.title.search(value) != -1;
+    });
+    setFilteredData(result);
+    }
+
+
+  
+  useEffect(() => {
+    axios('https://jsonplaceholder.typicode.com/albums/1/photos')
+    .then(response => {
+    console.log(response.data)
+    setAllData(response.data);
+    setFilteredData(response.data);
+    })
+    .catch(error => {
+    console.log('Error getting fake data: ' + error);
+    })
+    }, []);
+
+    const styles = {
+      display:'inline',
+      width:'30%',
+      height:50,
+      float:'left',
+      padding:5,
+      border:'0.5px solid black',
+      marginBottom:10,
+      marginRight:10
+      }
+
+
+
+
+
 
 
   return (
@@ -88,10 +133,35 @@ function App() {
 
       <body>
 
+
+
+
         <NameForm />
 
 
         {Seguimiento(a)}
+
+
+
+        <div style={{ margin: '0 auto', marginTop: '10%' }}>
+<label>Search:</label>
+<input type="text" onChange={(event) =>handleSearch(event)} />
+</div>
+<div style={{padding:10}}>
+{filteredData.map((value,index)=>{
+return(
+<div key={value.id}>
+<div style={styles} key={value.id}>
+{value.title}
+</div>
+</div>
+)
+})}
+</div>
+
+
+
+
         <div>
           aqui si busca no importa lo que sea que le envia por parametro
           no mas de pasarle el parametro
